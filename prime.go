f46d3e7b373998d6fcac42854cac9c9799288fd4
Praveen raj Mani <hackerpraveen@gmail.com>
2016-02-27 18:39:29 +0530

    Added files via upload
    formatted

--- prime.go
+++ prime.go
@@ -6,40 +6,32 @@ import "time"
 func main() {
 	t0 := time.Now()
 	lprime := 0
-        resulting := 0
-        
-	for i:=2 ;resulting<60 ;i++ {
-        if Is_prime(i) == 1 {
-                fmt.Println("\n",i)
-		lprime = i
-		t1 := time.Now()
-                resulting = int(t1.Sub(t0)/(time.Millisecond*1000))
-        	} else {
-        	t1 := time.Now()
-        	resulting = int(t1.Sub(t0)/(time.Millisecond*1000))
-        	}
+	resulting := 0
+
+	for i := 2; resulting < 60; i++ {
+		if validateprime(i) == 1 {
+			fmt.Println("\n", i)
+			lprime = i
+			t1 := time.Now()
+			resulting = int(t1.Sub(t0) / (time.Millisecond * 1000))
+		} else {
+			t1 := time.Now()
+			resulting = int(t1.Sub(t0) / (time.Millisecond * 1000))
+		}
 	}
-fmt.Println("\n\n The largest prime is ",lprime)
+	fmt.Println("\n\n The largest prime is ", lprime)
 }
 
-
-
-func Is_prime(number int) int {
-
+func validateprime(number int) int {
 	var count int = 0
-
-	for i:=1; i<=number; i++ {
-
+	for i := 1; i <= number; i++ {
 		if number%i == 0 {
 			count++
 		}
 	}
-
 	if count == 2 {
-	return 1
+		return 1
 	} else {
-	return 0
-	}	
+		return 0
+	}
 }
-
-
