34ba8a51a3f11e3008b38cc470ee536611b73ee0
PraveenrajMani <hackerpraveen@gmail.com>
2016-02-27 18:09:04 +0530

    Prime program in go

---
+++ prime.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+	t0 := time.Now()
+	lprime := 0
+        resulting := 0
+        
+	for i:=2 ;resulting<60 ;i++ {
+        if Is_prime(i) == 1 {
+                fmt.Println("\n",i)
+		lprime = i
+		t1 := time.Now()
+                resulting = int(t1.Sub(t0)/(time.Millisecond*1000))
+        	} else {
+        	t1 := time.Now()
+        	resulting = int(t1.Sub(t0)/(time.Millisecond*1000))
+        	}
+	}
+fmt.Println("\n\n The largest prime is ",lprime)
+}
+
+
+
+func Is_prime(number int) int {
+
+	var count int = 0
+
+	for i:=1; i<=number; i++ {
+
+		if number%i == 0 {
+			count++
+		}
+	}
+
+	if count == 2 {
+	return 1
+	} else {
+	return 0
+	}	
+}
+
+
